2b229b0533e8db6cf7483bcb032455922faa47b2
Maksym Polinka <Polinka7max@gmail.com>
2020-03-26 13:12:38 +0200

    Updates

---
+++ precache-manifest.235400d360ebd3c6a429333af08f07bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98a63dca1a17442b4e48c024d2357ec",
+    "url": "/movie-picker/index.html"
+  },
+  {
+    "revision": "587b6d16aae7803f830e",
+    "url": "/movie-picker/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "015d5525a45742492dfa",
+    "url": "/movie-picker/static/css/main.1e93d2cb.chunk.css"
+  },
+  {
+    "revision": "587b6d16aae7803f830e",
+    "url": "/movie-picker/static/js/2.7be208d3.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/movie-picker/static/js/2.7be208d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015d5525a45742492dfa",
+    "url": "/movie-picker/static/js/main.2f8044ba.chunk.js"
+  },
+  {
+    "revision": "66c5ccd034e9971fcfcf",
+    "url": "/movie-picker/static/js/runtime-main.593f545c.js"
+  },
+  {
+    "revision": "38615a2136e44e873c69ae4130c5e9e2",
+    "url": "/movie-picker/static/media/blockbuster-movies-1.38615a21.jpg"
+  },
+  {
+    "revision": "18a52d27427316551bb204b6547a41fe",
+    "url": "/movie-picker/static/media/classic-movies.18a52d27.jpg"
+  },
+  {
+    "revision": "ba36325c8259ec40960584381d613420",
+    "url": "/movie-picker/static/media/drama-movies.ba36325c.jpg"
+  },
+  {
+    "revision": "b3666bd182006aa2a4f3c7128757aca8",
+    "url": "/movie-picker/static/media/parallax-movies-bg.b3666bd1.jpg"
+  }
+]);
